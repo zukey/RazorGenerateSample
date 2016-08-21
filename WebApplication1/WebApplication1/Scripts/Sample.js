d2ae4255e23d1bd76d72098ff07e8c102b8c6dc8
zukey <valenciada2000@gmail.com>
2016-08-21 23:55:30 +0900

    サンプル

---
+++ WebApplication1/WebApplication1/Scripts/Sample.js
@@ -0,0 +1,5 @@
+﻿
+function Sample()
+{
+    console.log("sampleだよ");
+}
---
+++ WebApplication1/WebApplication2/Scripts/Own.js
@@ -0,0 +1,12 @@
+﻿
+$(function () {
+    $("#test").on('click', function () {
+        ownFunc();
+        Sample();
+    })
+});
+
+function ownFunc()
+{
+    console.log("自前だよ");
+}
